521a29c0eec87b8948e19c3054fbae67a07dc692
woshinivek <woshinivek@yahoo.com>
2021-12-29 09:16:31 +0300

    changes

--- js/01-basic-modal.js
+++ js/01-basic-modal.js
@@ -1,12 +1,12 @@
-const refs = {
-  openModalBtn: document.querySelector(".js-modal-btn"),
-};
+// const refs = {
+//   openModalBtn: document.querySelector(".js-modal-btn"),
+// };
 
-refs.openModalBtn.addEventListener("click", (ev) => {
-  const btnElement = ev.target;
+// refs.openModalBtn.addEventListener("click", (ev) => {
+//   const btnElement = ev.target;
 
-  console.dir(btnElement);
-});
+//   console.dir(btnElement);
+// });
 
 // // refs.openModalBtn.ontransitionend = () => {
 
