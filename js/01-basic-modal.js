6d1efb5495f1aa021f00571b6ee141137ab4139c
woshinivek <woshinivek@yahoo.com>
2021-12-30 21:57:20 +0300

    changes

--- js/01-basic-modal.js
+++ js/01-basic-modal.js
@@ -1,4 +1,33 @@
 const refs = {
   openModalBtn: document.querySelector(".js-open-modal-btn"),
   closeModalBtn: document.querySelector(".js-close-modal-btn"),
+  backdrop: document.querySelector(".backdrop"),
 };
+
+refs.openModalBtn.addEventListener("click", onOpenModalBtn);
+refs.closeModalBtn.addEventListener("click", onCloseModalBtn);
+refs.backdrop.addEventListener("click", onBackdropClick);
+
+function onOpenModalBtn(ev) {
+  document.body.classList.add("show-modal");
+
+  window.addEventListener("keydown", onEscKey);
+}
+
+function onCloseModalBtn(ev) {
+  document.body.classList.remove("show-modal");
+
+  window.removeEventListener("keydown", onEscKey);
+}
+
+function onBackdropClick(ev) {
+  if (ev.currentTarget === ev.target) {
+    onCloseModalBtn();
+  }
+}
+
+function onEscKey(ev) {
+  if (ev.code === "Escape") {
+    onCloseModalBtn();
+  }
+}
